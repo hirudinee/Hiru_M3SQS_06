830c056f78e3dd8955a4daea01a5e668da2e46ee
hirudinee <35219641+hirudinee@users.noreply.github.com>
2018-01-24 15:36:53 +0530

    S3 test

--- Hiru_M3SQS_06/lambda.js
+++ Hiru_M3SQS_06/lambda.js
@@ -1,4 +1,5 @@
 let AWS = require('aws-sdk');
+const s3 = new AWS.S3();
 let SL = require('@slappforge/slappforge-sdk');
 const sqs = new SL.AWS.SQS(AWS);
 exports.handler = function (event, context, callback) {
@@ -22,6 +23,26 @@ exports.handler = function (event, context, callback) {
 	}, function (error) {
 		// your logic (logging etc) to handle failures, should be here
 	});
+	s3.putObject({
+		"Body": "test",
+		"Bucket": "hiru.sample",
+		"Key": "test"
+	})
+		.promise()
+		.then(data => {
+			console.log(data);           // successful response
+			/*
+			data = {
+				ETag: "\\"6805f2cfc46c0f04559748bb039d69ae\\"", 
+				VersionId: "pSKidl4pHBiNwukdbcPXAIs.sshFFOc0"
+			}
+			*/
+		})
+		.catch(err => {
+			console.log(err, err.stack); // an error occurred
+		});
+
+
 
 	callback(null, 'Successfully executed');
 }
